4738848f896b823315929a528a6c984e3432acf3
Lucas Carvalho Dos Santos <lucascarvalho22056@outlook.com>
2023-05-04 08:58:53 -0300

    "refactor: add hashpassword method and partial toResponse method"

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert } from "typeorm"
+import {hash} from 'bcrypt'
 
 @Entity('users')
 export class User {
@@ -14,9 +15,25 @@ export class User {
     @Column({type : 'varchar'})
     password: string
 
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await hash(this.password, 12)
+    }
+
     @CreateDateColumn()
     createdAt: Date;
   
     @UpdateDateColumn()
     updatedAt: Date;
+
+    toResponse(): Partial<User> {
+        const responseUser = new User();
+        responseUser.id = this.id;
+        responseUser.userName = this.userName;
+        responseUser.email = this.email;
+        responseUser.createdAt = this.createdAt;
+        responseUser.updatedAt = this.updatedAt;
+
+        return responseUser
+    }
 }
