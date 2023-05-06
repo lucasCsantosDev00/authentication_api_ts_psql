640acb3ac4bb15309ed3237b23ec2a1bfb230262
Lucas Carvalho Dos Santos <lucascarvalho22056@outlook.com>
2023-05-06 09:55:42 -0300

    refactor: add auth routes and ErrorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv'
 import express, {Express, Request, Response} from "express"
 import bodyParser from 'body-parser'
 import cors from "cors"
+import authRoute from './routes/authRoutes'
+import { ErrorHandler } from './http/middlewares/ErrorHandler'
 
 dotenv.config()
 
@@ -11,5 +13,16 @@ const app: Express = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.use("/auth", authRoute)
+
+app.use("*", (req: Request, res: Response) => {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid route",
+    });
+  });
+  
+app.use(ErrorHandler.handleErrors);
 
 export default app;
