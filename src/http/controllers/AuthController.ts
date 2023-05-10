e91590d501e3f9c4adfcd1b3a46e41d85935de14
Lucas Carvalho Dos Santos <lucascarvalho22056@outlook.com>
2023-05-09 21:16:27 -0300

    feat: add login  method to AuthController

--- src/http/controllers/AuthController.ts
+++ src/http/controllers/AuthController.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import { SignUpDTO } from "../dtos/AuthDTO";
+import { SignUpDTO, LoginDTO } from "../dtos/AuthDTO";
 import { validateOrReject } from "class-validator";
 import AppDataSource from "@/database/db.config";
 import { User } from "@/database/entities/user.entity";
 import { ResponseUtil } from "@/utils/Response";
+import {compare} from 'bcryptjs'
+import {sign} from "jsonwebtoken"
 
 export class AuthController {
     async signUp(req: Request, res: Response, next: NextFunction): Promise<Response> {
@@ -24,5 +26,32 @@ export class AuthController {
 
         return ResponseUtil.sendResponse(res, 'Sign up succesful!', user)
         
+    }
+
+    async login(req: Request, res: Response, next: NextFunction) {
+        const { email, password } = req.body;
+
+        const dto = new LoginDTO()
+        dto.email = email
+        dto.password = password
+
+        await validateOrReject(dto)
+
+        const repository = AppDataSource.getRepository(User)
+        const user = await repository.findOneBy({email})
+        if(!user) {
+            return ResponseUtil.sendError(res, "Invalid Credentials", 401, null)
+        }
+        let passwordMatches = await compare(password, user.password)
+
+        if(!passwordMatches) {
+            return ResponseUtil.sendError(res, "Invalid credentials", 401, null)
+        }
+        let accessToken = sign({userId:user.id}, process.env.ACCESS_KEY_SECRET || "secret123", {expiresIn: "30m",})
+
+        const returnUser = user.toResponse();
+
+        return ResponseUtil.sendResponse(res, "User login success", { user: returnUser, accessToken });
+
     }
 }
